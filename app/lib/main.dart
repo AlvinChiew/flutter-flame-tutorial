68500b34418fbd41d1f6f6e95bedcd9d22dd2f33
alvinchiew <jingjie95@gmail.com>
2024-10-13 02:17:32 +0800

    Add audio tutorial

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/02_joystick/game.dart';
+import 'package:app/03_audio/game.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
